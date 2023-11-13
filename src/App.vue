427f47fecbb1a07c427c7c32751302d86450981a
misha-sv <mikhail.svekolnikov@kultprosvet.net>
2023-11-13 13:37:24 +0200

    Make slots named with props

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Component1 from './Component1.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<div>
+
+    <Component1 >
+      <template v-slot:buttonSlot="slotProps">
+        <Button @click="console.log(slotProps.text)">Click {{slotProps.text}}</Button>
+      </template>
+
+      <template v-slot:buttonHeaderSlot="slotProps">
+        <h1>Click {{slotProps.text}}</h1>
+      </template>
+    </Component1>
+
+
+</div>
+</template>
---
+++ src/Component1.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Component2 from "./Component2.vue";
+</script>
+
+<template>
+<div>
+  <div>Component child =></div>
+
+  <slot name="buttonHeaderSlot" :text="'Headeeer'"></slot>
+  <slot name="buttonSlot" :text="'text'"></slot>
+
+
+<!--  <Component2 />-->
+</div>
+</template>
