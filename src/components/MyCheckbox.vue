10bfc62e5f9174fa4ce43dd067781c05631edecf
misha-sv <mikhail.svekolnikov@kultprosvet.net>
2023-11-19 23:37:53 +0200

    Checkbox table

---
+++ src/components/MyCheckbox.vue
@@ -0,0 +1,61 @@
+<template>
+  <label :for="label" class="checkbox-label">
+    <input
+      :id="label"
+      type="checkbox"
+      v-model="model"
+      :value="value"
+      class="checkbox-input"
+    />
+
+    <span class="label-text">{{ label }} </span>
+  </label>
+</template>
+
+<script setup>
+import { computed, defineEmits } from "vue";
+const props = defineProps({
+  modelValue: { type: [Array, Boolean] },
+  value: { type: [Boolean, Object] },
+  label: { type: String },
+});
+const emit = defineEmits(["update:modelValue"]);
+const model = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  },
+});
+</script>
+
+<style scoped>
+label {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+.checkbox-input {
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  width: 20px;
+  height: 20px;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-right: 10px;
+  cursor: pointer;
+}
+
+.checkbox-input:checked {
+  background-color: #007bff;
+  border: 2px solid #007bff;
+}
+
+.label-text {
+  font-size: 16px;
+}
+</style>
--- src/views/TableCheckbox.vue
+++ src/views/TableCheckbox.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>TableCheckbox view</div>
+  <div>
+    <div v-for="category in categories" key="person.value">
+      <MyCheckbox
+        v-model="selectedCategories"
+        :value="category.value"
+        :label="category.label"
+      />
+    </div>
+
+    <p>Using the custom checkbox with an array</p>
+    <p>{{ selectedCategories }}</p>
+
+    <MyCheckbox v-model="isSelected" label="One checkbox" />
+    <div>{{ isSelected }}</div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import MyCheckbox from "../components/MyCheckbox.vue";
+
+const categories = ref([
+  { value: 1, label: "Technology" },
+  { value: 2, label: "Movies" },
+  { value: 3, label: "Food" },
+  { value: 4, label: "Animals" },
+]);
+
+const selectedCategories = ref([]);
+const isSelected = ref(false);
+</script>
