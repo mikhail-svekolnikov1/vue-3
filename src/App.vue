111a6c06182687b89a769aea6c9e11c733bba856
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-12-17 11:33:28 +0200

    test change

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 <template>
   <div>
     <router-link to="/">Go to Home</router-link>
-    <router-link to="/about">Go to About</router-link>
+    <router-link to="/about">Go to About </router-link>
     <router-view></router-view>
   </div>
 </template>
