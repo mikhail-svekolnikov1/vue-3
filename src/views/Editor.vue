498487ef044bc69140025f9cd484b4e64bffdd61
misha-sv <mikhail.svekolnikov@kultprosvet.net>
2024-04-02 08:32:06 +0300

    Quill editor

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,273 +1,93 @@
 <template>
   <div class="ui-text-editor">
-    <div class="ui-text-editor__tool-bar">
-      <button
-        type="button"
-        @click="modify('bold')"
-        class="ui-text-editor__modify-btn"
-      >
-        bold
-      </button>
-
-      <button
-        type="button"
-        @click="modify('italic')"
-        class="ui-text-editor__modify-btn"
-      >
-        italic
-      </button>
-    </div>
-
-    <div
-      id="editable"
-      class="ui-text-editor__content"
-      contenteditable="true"
-      ref="contentEl"
-      :placeholder="placeholder"
-      tabindex="0"
-      @keyup="emitUpdate"
-    ></div>
-    <!--    <div class="tag-dropdown" v-if="showDropdown">cat misha</div>-->
-    <teleport to="body" v-if="showDropdown">
-      <div
-        class="tag-dropdown"
-        v-bind:style="{ top: cursorPosition.top, left: cursorPosition.left }"
-      >
-        <div
-          class="tag-dropdown-item"
-          v-for="option in optionsForTags"
-          @click="onChooseTag(option)"
-        >
-          {{ option }}
-        </div>
-      </div>
-    </teleport>
+    <QuillEditor
+      ref="myEditor"
+      v-model:content="model"
+      theme="snow"
+      :modules="modules"
+      contentType="html"
+      @input="onInput"
+      @update:content="textChange"
+      @select="select"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from "vue";
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import QuillMention from "quill-mention";
+import "quill-mention/dist/quill.mention.css";
+import { ref, watch } from "vue";
 
-import type { Ref } from "vue";
+const model = ref("hello");
+const myEditor = ref();
 
-const props = withDefaults(
-  defineProps<{
-    modelValue: string;
-    placeholder?: string;
-    optionsForTags?: string[];
-  }>(),
-  {
-    placeholder: "",
-    optionsForTags: ["@firstName", "@lastName", "@companyName"],
-  }
-);
+const atValues = [
+  { id: 1, value: "firstName" },
+  { id: 2, value: "lastName" },
+  { id: 3, value: "companyName" },
+];
 
-const emit = defineEmits<{
-  (e: "update:modelValue", value: string): void;
-}>();
-
-const contentEl: Ref<HTMLElement | null> = ref(null);
-const showDropdown = ref<boolean>(false);
-const isDropdownInTyping = ref<boolean>(false);
-const cursorPositionForInsertTag = ref<number>(0);
-const cursorPosition = ref({
-  left: "0px",
-  top: "0px",
-});
-
-watchEffect(() => {
-  if (contentEl.value && contentEl.value.innerHTML !== props.modelValue) {
-    contentEl.value.innerHTML = props.modelValue || "";
-  }
-});
-
-const emitUpdate = (event) => {
-  const selection = window.getSelection();
-  const range = selection.getRangeAt(0);
-
-  const rect = range.getBoundingClientRect();
-
-  // const text = event.target.textContent;
-  // const lastChar = text.charAt(text.length - 1);
-  // console.log("event", event);
-  // console.log("all lettes", contentEl.value?.innerHTML.length);
-  // console.log("selection focusOffset:", selection.focusOffset);
-  cursorPositionForInsertTag.value = selection.focusOffset;
-  console.log("selection.focusOffset", selection.focusOffset);
-  if (event.key === "@") {
-    const t = rect.top + window.scrollY + 20;
-    const l = rect.left + window.scrollX - 10;
-
-    const top = `${t}px`;
-    const left = `${l}px`;
-
-    cursorPosition.value = {
-      top,
-      left,
-    };
-
-    showDropdown.value = true;
-    isDropdownInTyping.value = true;
-  }
-
-  if (isDropdownInTyping.value && event.key === " ") {
-    showDropdown.value = false;
-    isDropdownInTyping.value = false;
-  } else if (!isDropdownInTyping.value) {
-    showDropdown.value = false;
-  }
-
-  emit("update:modelValue", contentEl.value?.innerHTML || "");
+// const hashValues = [
+//   { id: 3, value: "Fredrik Sundqvist 2" },
+//   { id: 4, value: "Patrik Sjölin 2" },
+// ];
+const select = (value) => {
+  console.log(value);
+  // console.log("contents", myEditor.value.getContents());
 };
 
-const modify = (commandName: string): void => {
-  document.execCommand(commandName);
+const textChange = (value) => {
+  function stripHtmlTags(htmlString) {
+    // Регулярное выражение для поиска и замены HTML-тегов
+    const regex = /(<([^>]+)>)/gi;
 
-  emitUpdate();
+    // Замена всех HTML-тегов на пустую строку
+    const plainText = htmlString.replace(regex, "");
+
+    return plainText;
+  }
+
+  const content = myEditor.value.getContents();
+
+  console.log("result", stripHtmlTags(content));
 };
 
-const onChooseTag = (tagText: string) => {
-  showDropdown.value = false;
-  isDropdownInTyping.value = false;
+const modules = {
+  name: "QuillMention",
+  module: QuillMention,
+  formats: ["bold", "italic", "mention"],
+  options: {
+    allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+    mentionDenotationChars: ["@", "#"],
+    source: function (searchTerm, renderList, mentionChar) {
+      let values;
 
-  // contentEl.value.innerHTML = `${contentEl.value?.innerHTML}${tagText}`;
+      if (mentionChar === "@") {
+        values = atValues;
+      } else {
+        // values = hashValues;
+      }
 
-  // Получаем текущее значение элемента contenteditable
-  const currentValue = contentEl.value.innerHTML;
+      if (searchTerm.length === 0) {
+        renderList(values, searchTerm);
+      } else {
+        const matches = [];
+        for (let i = 0; i < values.length; i++)
+          if (~values[i].value.toLowerCase().indexOf(searchTerm.toLowerCase()))
+            matches.push(values[i]);
 
-  // Получаем позицию вставки
-  const position = cursorPositionForInsertTag.value;
+        console.log("matches", matches);
+        console.log("searchTerm", searchTerm);
 
-  // -1 to remove double '@'
-  const textBeforeTag = currentValue.slice(0, position - 1);
-  const textAfterTag = currentValue.slice(position);
-
-  const newText = `${textBeforeTag}${tagText}${textAfterTag}`;
-
-  contentEl.value.innerHTML = newText;
-
-  contentEl.value.focus();
-
-  let sel = window.getSelection();
-  let focus = sel.focusNode;
-
-  let range = document.createRange();
-  range.selectNode(focus);
-
-  // console.log("length of text", contentEl.value?.innerHTML.length);
-  // console.log("tagText.length", tagText.length);
-  // console.log("position", position);
-
-  range.setStart(focus, position + tagText.length - 1);
-
-  // if (contentEl.value?.innerHTML.length > position + tagText.length) {
-  //   range.setStart(focus, contentEl.value?.innerHTML.length);
-  // } else {
-  //   range.setStart(focus, position + tagText.length);
-  // }
-
-  range.collapse(true);
-  sel.removeAllRanges();
-  sel.addRange(range);
+        renderList(matches, searchTerm);
+      }
+    },
+    renderItem: (value) => {
+      console.log(value);
+      return `@${value.value}`;
+    },
+  },
 };
 </script>
-
-<style lang="scss">
-@import "./_variables.scss";
-
-.tag-dropdown {
-  position: absolute;
-  border: 1px solid $color-gray-02;
-  border-radius: 5px;
-  background-color: white;
-
-  .tag-dropdown-item {
-    border: 5px;
-    padding: 16px 14px;
-    font-weight: 500;
-    border-bottom: 1px solid $color-gray-02;
-    cursor: pointer;
-
-    &:hover {
-      background: $color-gray-01;
-    }
-
-    &:last-child {
-      border-bottom: none;
-    }
-  }
-}
-
-.ui-text-editor {
-  position: relative;
-
-  width: 100%;
-  border-radius: 12px;
-  border: solid 2px $color-gray-02;
-
-  &__tool-bar {
-    height: 52px;
-    display: flex;
-    align-items: center;
-    border-bottom: solid 2px $color-gray-02;
-    padding: 0 24px;
-
-    button:focus {
-      outline: solid 1px #ddd;
-    }
-  }
-
-  &__modify-btn {
-    padding: 8px;
-
-    &:hover {
-      background: $color-gray-01;
-    }
-
-    svg {
-      fill: #000;
-    }
-
-    &--ms {
-      margin-inline-start: 30px;
-    }
-  }
-
-  &__content {
-    height: 220px;
-    overflow-y: auto;
-    padding: 18px 20px;
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 22px;
-    color: $color-gray-07;
-    text-align: start;
-  }
-}
-
-@media screen and (max-width: 340px) {
-  .ui-text-editor__tool-bar {
-    height: auto;
-    min-height: 52px;
-    flex-wrap: wrap;
-    padding: 0 10px;
-  }
-}
-
-.ui-text-editor ul {
-  list-style-type: disc !important;
-  padding: 10px 25px;
-}
-
-.ui-text-editor ol {
-  list-style-type: decimal !important;
-  padding: 10px 25px;
-}
-
-[placeholder]:empty::before {
-  content: attr(placeholder);
-  font-size: 16px;
-  font-weight: 400;
-  color: $color-gray-07;
-}
-</style>
